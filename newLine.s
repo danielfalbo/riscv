94792741558640c4df4057c62e0222b702da3d8d
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-03-13 12:05:52 +0100

    Add newLine.s

---
+++ newLine.s
@@ -0,0 +1,35 @@
+# function prints a new line to the console
+
+.text
+    # print 1
+    li a7, 1
+    li a0, 1
+    ecall
+
+    # print new line
+    jal ra, newLine
+
+    # print 0
+    li a7, 1
+    li a0, 0
+    ecall
+
+    # print new line
+    jal ra, newLine
+
+    # exit gracefully
+    addi a7, zero, 10
+    ecall
+
+    newLine:
+        # li: load immediate
+        #       uses addi, ori, or something like that
+        #       under the hood
+        # 10: ascii code for "\n"
+        li a0, 10
+        # 11: system call for "print character"
+        li a7, 11
+        ecall
+
+        # jump back to where we left
+        jalr zero, ra, 0
