bd91209917ed126b8a8e6332dd4138b8da7e8d2c
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-04-14 18:37:13 +0200

    Add `recursive_function_linked_list_length.s`

---
+++ recursive_function_linked_list_length.s
@@ -0,0 +1,39 @@
+.data:
+    head: .word n01
+    # head: .word 0
+
+    # n01:    .word 0, 0
+    n01:   .word 3, n02
+    n02:   .word 7, n03
+    n03:   .word 6, n04
+    n04:   .word 2, n05
+    n05:   .word 6, 0
+
+.text:
+    lw a0, head
+    jal ra, f
+    beq zero, zero, exit
+
+    f:
+        beq a0, zero, return
+
+        addi sp, sp, -4
+        sw ra, 0(sp)
+
+        lw a0, 4(a0)
+        jal ra, f
+
+        addi a0, a0, 1
+
+        lw ra, 0(sp)
+        addi sp, sp, 4
+        jalr zero, 0(ra)
+
+    return:
+        jalr zero, 0(ra)
+
+    exit:
+        li a7, 1
+        ecall
+        li a7, 10
+        ecall
